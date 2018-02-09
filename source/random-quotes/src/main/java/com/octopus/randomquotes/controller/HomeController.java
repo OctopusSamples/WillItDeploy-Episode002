532591aa246f84d474f25dc15ea33446ca9e7377
Rob Pearson <rob.pearson@me.com>
2018-02-09 15:49:25 +1000

    Added application properties.

--- source/random-quotes/src/main/java/com/octopus/randomquotes/controller/HomeController.java
+++ source/random-quotes/src/main/java/com/octopus/randomquotes/controller/HomeController.java
@@ -1,14 +1,22 @@
 package com.octopus.randomquotes.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Value("${environment.name}")
+    private String environmentName;
+
     @RequestMapping("/")
     public String home() {
-        return "Hello world";
+
+        return "Running " + appVersion + " in " + environmentName;
     }
 
 }
--- source/random-quotes/src/test/java/com/octopus/randomquotes/RandomQuotesApplicationTests.java
+++ source/random-quotes/src/test/java/com/octopus/randomquotes/RandomQuotesApplicationTests.java
@@ -11,6 +11,7 @@ public class RandomQuotesApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
