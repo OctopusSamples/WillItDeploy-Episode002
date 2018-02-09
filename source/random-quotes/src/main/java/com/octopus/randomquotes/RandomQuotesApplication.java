52f5b40eb6f92acd8db7fb33b5430ccbe76507e6
Rob Pearson <rob.pearson@me.com>
2018-02-09 13:28:51 +1000

    Initial commit.

---
+++ source/random-quotes/src/main/java/com/octopus/randomquotes/RandomQuotesApplication.java
@@ -0,0 +1,12 @@
+package com.octopus.randomquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomQuotesApplication.class, args);
+	}
+}
